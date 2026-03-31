785331ad5f5f968f1a938d67afa11dd49f4aab37
Victor Olusegun <victorolusegun003@gmail.com>
2026-03-31 21:32:53 +0100

    Added feature to read PDF file

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import pdfplumber
 from src import *
 
 print('Welcome to the Python app!')
+
+with pdfplumber.open("C:\\Users\\hp\\Desktop\\Restart\\POS Profit Calculator\\data\\raw\\Daily_Statement.pdf") as pdf:
+    for page in pdf.pages:
+        table = page[0].extract_text().split()
+        print(table)
