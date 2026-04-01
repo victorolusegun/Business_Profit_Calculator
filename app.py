1983f6679fcd401ba830bea2afef5a636227a707
Victor Olusegun <victorolusegun003@gmail.com>
2026-04-01 21:53:37 +0100

    Extraction from pdf finally works but still needs refinement

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import pdfplumber
 from src import *
 
 print('Welcome to the Python app!')
-
-with pdfplumber.open("C:\\Users\\hp\\Desktop\\Restart\\POS Profit Calculator\\data\\raw\\Daily_Statement.pdf") as pdf:
-    for page in pdf.pages:
-        table = page[0].extract_text().split()
-        print(table)
+file_path = r"C:\\Users\\hp\\Desktop\\Restart\\POS Profit Calculator\\data\\raw\\Daily_Statement.pdf"
+with pdfplumber.open(file_path) as pdf:
+    page = pdf.pages[0]
+    table = page.extract_text()
+    for line in table.split('\n'):
+        line_list = line.split()
+        print(line_list)
+    # print(table)
+    # for page in pdf.pages:
+    #     table = page.extract_text().split('\n')
+    #     print(table)
